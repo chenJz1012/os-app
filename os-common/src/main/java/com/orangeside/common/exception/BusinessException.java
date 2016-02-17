700a8f9efa57dd45d845d12e177f2808a4ed0c28
陈国俊 <597160667@qq.com>
2016-02-17 19:56:02 +0800

    方法名更正

--- os-common/src/main/java/com/orangeside/common/exception/BusinessException.java
+++ os-common/src/main/java/com/orangeside/common/exception/BusinessException.java
@@ -12,20 +12,9 @@ public class BusinessException extends RuntimeException {
     private static final long serialVersionUID = 4793699346343027629L;
     private String PROPERTIES = "/config/properties/business_exception.properties";
     private String err = "";
-
     public BusinessException(int errCode) {
         this.err = PropertiesUtil.get(PROPERTIES, String.valueOf(errCode));
     }
-
-    /**
-     * 不建议使用该方法，容易造成代码维护问题
-     * @param message
-     */
-    @Deprecated
-    public BusinessException(String message) {
-        this.err = message;
-    }
-
     @Override
     public String getMessage() {
         return this.err;
